eacb56ed6e4a266da73cd09c67c71cf9b3d0a654
shinchandler <pranavrishi8@gmail.com>
2023-10-20 10:42:42 +0530

    first commit

---
+++ Watermelon.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+
+        if (w % 2 == 0 && w > 2) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
